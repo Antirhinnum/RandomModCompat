de53e8efb8af9b2f2a7e0ee648aeea71279fa78a
Michael Scott <michaelcscott2022@gmail.com>
2023-04-13 20:51:18 -0400

    Added `CrossModHandler` and `CrossModSystem`

---
+++ Core/CrossModHandler.cs
@@ -0,0 +1,60 @@
+﻿using Terraria.ModLoader;
+
+namespace RandomModCompat.Core;
+
+/// <summary>
+/// Handles basic cross-mod call support.
+/// </summary>
+internal abstract class CrossModHandler : ILoadable
+{
+	/// <summary>
+	/// The mod that adds this handler.
+	/// </summary>
+	protected Mod Mod { get; private set; }
+
+	/// <summary>
+	/// The mod this handler adds support for.
+	/// </summary>
+	protected Mod CrossMod => ModLoader.TryGetMod(ModName, out Mod result) ? result : null;
+
+	/// <summary>
+	/// Checks if the mod named <see cref="ModName"/> is currently loaded.<br/>
+	/// <b>If <see langword="false"/>, then no method in this class will be called.</b>
+	/// </summary>
+	internal bool IsModLoaded => CrossMod != null;
+
+	/// <summary>
+	/// The internal name of the mod this handler adds support for.
+	/// </summary>
+	protected abstract string ModName { get; }
+
+	/// <inheritdoc cref="ModSystem.OnModLoad"/>
+	internal virtual void OnModLoad()
+	{ }
+
+	/// <inheritdoc cref="ModSystem.SetupContent"/>
+	internal virtual void SetupContent()
+	{ }
+
+	/// <inheritdoc cref="ModSystem.PostSetupContent"/>
+	internal virtual void PostSetupContent()
+	{ }
+
+	/// <summary>
+	/// Allows loading after everything else has been loaded, including localization.<br/>
+	/// This runs after <see cref="ModSystem.PostAddRecipes"/>, so some mods may disallow calls at this point.
+	/// </summary>
+	/// <param name="mod"></param>
+	internal virtual void PostSetupEverything()
+	{ }
+
+	void ILoadable.Load(Mod mod)
+	{
+		Mod = mod;
+		CrossModSystem._handlers.Add(this);
+	}
+
+	void ILoadable.Unload()
+	{
+	}
+}
---
+++ Core/CrossModSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace RandomModCompat.Core;
+
+/// <summary>
+/// Handles cross-mod call support.
+/// </summary>
+public sealed partial class CrossModSystem : ModSystem
+{
+	internal static readonly List<CrossModHandler> _handlers = new();
+
+	public override void OnModLoad()
+	{
+		foreach (CrossModHandler handler in _handlers)
+		{
+			if (handler.IsModLoaded)
+			{
+				handler.OnModLoad();
+			}
+		}
+	}
+
+	public override void SetupContent()
+	{
+		foreach (CrossModHandler handler in _handlers)
+		{
+			if (handler.IsModLoaded)
+			{
+				handler.SetupContent();
+			}
+		}
+	}
+
+	public override void PostSetupContent()
+	{
+		foreach (CrossModHandler handler in _handlers)
+		{
+			if (handler.IsModLoaded)
+			{
+				handler.PostSetupContent();
+			}
+		}
+	}
+
+	public override void PostAddRecipes()
+	{
+		foreach (CrossModHandler handler in _handlers)
+		{
+			if (handler.IsModLoaded)
+			{
+				handler.PostSetupEverything();
+			}
+		}
+	}
+
+	public override void Unload()
+	{
+		_handlers.Clear();
+	}
+}
