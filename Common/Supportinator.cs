93cce522c400cb6e46f66ad17e18ef8a9b81e148
Michael Scott <michaelcscott2022@gmail.com>
2023-05-25 11:22:45 -0400

    Supportinator the First
    * Added a very basic version of the Supportinator.

---
+++ Common/Supportinator.cs
@@ -0,0 +1,42 @@
+﻿using RandomModCompat.Utilities;
+using System.Diagnostics;
+using System.Reflection;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
+
+namespace RandomModCompat.Common;
+
+/// <summary>
+/// A simple checker for mod support.
+/// </summary>
+internal sealed class Supportinator : ModSystem
+{
+	private static ModSupportConfig Config => ModContent.GetInstance<ModSupportConfig>();
+
+	public override bool IsLoadingEnabled(Mod mod)
+	{
+		return Debugger.IsAttached;
+	}
+
+	// Runs after PostSetupContent().
+	public override void PostSetupRecipes()
+	{
+		FishingChecker();
+	}
+
+	// Fishing Reborn
+	private void FishingChecker()
+	{
+		Mod.Logger.Debug("Checking fishing methods...");
+		MethodInfo catchFish = typeof(ModPlayer).GetMethod(nameof(ModPlayer.CatchFish), ReflectionHelper.AllFlags);
+		foreach (ModPlayer player in ModContent.GetContent<ModPlayer>().WhereMethodIsOverridden(catchFish))
+		{
+			string modName = player.Mod.Name;
+			Mod.Logger.Debug($"Hit! Mod: \"{modName}\" | ModPlayer: \"{player.Name}\"");
+			if (!Config.SupportEnabled(modName, ModNames.FishingReborn))
+			{
+				Mod.Logger.Debug("Missing FishingReborn support!");
+			}
+		}
+	}
+}
